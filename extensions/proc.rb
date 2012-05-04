009e9b920ac5bf923b7d92108434e1e89e99df01
meussram <samuel.ramambason@gmail.com>
2012-05-04 13:14:35 -0700

    adding credits

--- extensions/proc.rb
+++ extensions/proc.rb
@@ -1,5 +1,8 @@
 class Proc
   
+  # Creates dynamic callbacks. Courtesy of Matt Sears. 
+  # http://www.mattsears.com/articles/2011/11/27/ruby-blocks-as-dynamic-callbacks
+  # http://techscursion.com/2011/11/turning-callbacks-inside-out
   def callback(callable, *args)
     self === Class.new do
     method_name = callable.to_sym
