13e9e550fd43d45e28dc42b7785879fb8b360c1d
meussram <samuel.ramambason@gmail.com>
2012-05-04 13:19:10 -0700

    added comments to resquer.rb

--- scripts/resquer.rb
+++ scripts/resquer.rb
@@ -1,6 +1,8 @@
 require 'resque'
 require File.expand_path('../../lib/audio_file_parser', __FILE__)
 
+# Array of audio files.
+# Uploaded files, or files read from whatever resource are enqueued as shown below
 files = ['102067__lg__waterfall-saas-fee-catchment-south-big-01-100609.wav', '3373__suonho__cartoonist_03_micious_suonho_.wav']
 
 files.each do |file|
