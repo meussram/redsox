468b8bf6f42c7688535fa3bd122c0a3c1aca7172
meussram <samuel.ramambason@gmail.com>
2012-05-07 14:33:59 -0700

    merging Sinatra routes

--- server.rb
+++ server.rb
@@ -1,32 +1,65 @@
+require 'rubygems'
 require 'sinatra'
-require 'redis'
+require 'sinatra/reloader'
+require 'sinatra/base' 
 require 'YAML'
-require "#{File.dirname(__FILE__)}/wrappers/redis_instance"
+require 'resque'
+require File.expand_path('../lib/audio_file_parser', __FILE__)
 
-configure { set :server, :puma}
-set :port, 9118
-
-redis = Redis.new(RedisInstance.config)
-
-
-get '/:id/plots' do
-	audio_json = redis.hget("audio_files:#{params[:id]}", "json_plots")
-end
-
-
-get '/:id/timestamps' do 
-	timestamps = redis.hget("audio_files:#{params[:id]}", "timestamps")
-end
-
-
-get '/:id/metadata' do
-  meta={}
+class MyApp < Sinatra::Base
   
-  keys = redis.hkeys("audio_files:#{params[:id]}")
-  keys.each do |k|
-    unless k == "json_plots" or k == "timestamps"
-      meta[k] = redis.hget("audio_files:#{params[:id]}", "#{k}")
-    end
+  configure { set :server, :puma }
+
+	set :static, true
+	set :public, File.dirname(__FILE__) + '/public'
+	enable :sessions
+
+
+  get '/' do
+    erb :index
   end
-  meta.inspect
+  
+
+  get '/upload' do
+    erb :upload
+  end
+  
+
+  post '/upload' do
+    configuration_root_dir = File.join(File.dirname(__FILE__),"","conf")
+    resources_config_file = YAML::load_file("#{configuration_root_dir}/resources.yml")
+    @audio_files_dir = "#{File.join(File.dirname(__FILE__),"","#{resources_config_file["audio_files"]}")}"
+ 
+    File.open("#{@audio_files_dir}/" + params['sound'][:filename], "w") do |f|
+      f.write(params['sound'][:tempfile].read)
+      Resque.enqueue(AudioFileParser, params['sound'][:filename])
+    end
+
+  end
+  
+
+  get '/:id/plots' do
+  	audio_json = redis.hget("audio_files:#{params[:id]}", "json_plots")
+  end
+
+
+  get '/:id/timestamps' do 
+  	timestamps = redis.hget("audio_files:#{params[:id]}", "timestamps")
+  end
+
+
+  get '/:id/metadata' do
+    meta={}
+
+    keys = redis.hkeys("audio_files:#{params[:id]}")
+    keys.each do |k|
+      unless k == "json_plots" or k == "timestamps"
+        meta[k] = redis.hget("audio_files:#{params[:id]}", "#{k}")
+      end
+    end
+    meta.inspect
+  end
+  
 end
+
+MyApp.run!  #if $0 == __FILE__
